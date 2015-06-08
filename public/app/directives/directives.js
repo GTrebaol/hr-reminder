4e77e0eaf5c95cbb906b89b24354cc8df827704c
Trebaol Gwenaël <trebaol.gwenael@gmail.com>
2015-06-08 11:20:34 +0200

    Added a clock displayed at any time on the menu

---
+++ public/app/directives/directives.js
@@ -0,0 +1,35 @@
+// This file will contains all the different directives we might need for the application
+
+
+var myOwnClock = function($filter){
+  return function(scope, element, attrs){
+      var format;
+
+      scope.$watch(attrs.myOwnClock, function(value) {
+        if(value){
+          format = value;
+        }else{
+          format = " dd/MM/yyyy  hh:mm:ss"
+        }
+          updateTime();
+      });
+
+      function updateTime(){
+          var dt = $filter('date')(new Date(), format);
+          element.text(dt);
+      }
+
+      function updateLater() {
+          setTimeout(function() {
+            updateTime(); // update DOM
+            updateLater(); // schedule another update
+          }, 1000);
+      }
+      updateLater();
+  }
+}
+
+
+myOwnClock.$inject = ['$filter'];
+
+angular.module('hrReminder').directive('myOwnClock', myOwnClock);
