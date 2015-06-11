92768b7cf015b6f2169816ecbed251583222daac
Trebaol Gwenaël <trebaol.gwenael@gmail.com>
2015-06-11 17:49:43 +0200

    modification on the model (adding column), added data for the generation

--- utils/sql/1.create_schema.sql
+++ utils/sql/1.create_schema.sql
@@ -5,13 +5,16 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema reminder
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema reminder
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `reminder` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `reminder` DEFAULT CHARACTER SET utf8 ;
 USE `reminder` ;
 
 -- -----------------------------------------------------
@@ -20,27 +23,30 @@ USE `reminder` ;
 DROP TABLE IF EXISTS `reminder`.`utilisateur` ;
 
 CREATE TABLE IF NOT EXISTS `reminder`.`utilisateur` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(80) NOT NULL,
   `prenom` VARCHAR(80) NOT NULL,
-  `telephone` VARCHAR(20) NULL,
-  `mail` VARCHAR(100) NULL,
-  `discr` ENUM('candidat', 'independant', 'apsidien') NOT NULL,
-  `formation` VARCHAR(80) NULL,
-  `annees_etudes` INT NULL,
-  `competences` VARCHAR(150) NULL,
-  `pretentions` INT NULL,
-  `mobilite` VARCHAR(45) NULL,
-  `remarques` VARCHAR(200) NULL,
-  `decision` TINYINT(1) NULL,
-  `source` VARCHAR(80) NULL,
-  `date_embauche` DATE NULL,
-  `dernier_ese` DATE NULL,
-  `prochain_ese` DATE NULL,
-  `client_actuel` VARCHAR(80) NULL,
+  `telephone` VARCHAR(20) NULL DEFAULT NULL,
+  `mail` VARCHAR(100) NULL DEFAULT NULL,
+  `discr` ENUM('candidat','independant','apsidien') NOT NULL,
+  `formation` VARCHAR(80) NULL DEFAULT NULL,
+  `annees_etudes` INT(11) NULL DEFAULT NULL,
+  `competences` VARCHAR(150) NULL DEFAULT NULL,
+  `pretentions` INT(11) NULL DEFAULT NULL,
+  `mobilite` VARCHAR(45) NULL DEFAULT NULL,
+  `remarques` VARCHAR(500) NULL DEFAULT NULL,
+  `decision` VARCHAR(80) NULL DEFAULT NULL,
+  `source` VARCHAR(80) NULL DEFAULT NULL,
+  `date_embauche` DATE NULL DEFAULT NULL,
+  `dernier_ese` DATE NULL DEFAULT NULL,
+  `prochain_ese` DATE NULL DEFAULT NULL,
+  `client_actuel` VARCHAR(80) NULL DEFAULT NULL,
+  `dernier_rdv` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 14
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -49,21 +55,23 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `reminder`.`rappel` ;
 
 CREATE TABLE IF NOT EXISTS `reminder`.`rappel` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `date_rappel` DATE NOT NULL,
-  `date_rdv` DATE NULL,
-  `commentaire` VARCHAR(150) NULL,
-  `traite` TINYINT(1) NOT NULL DEFAULT 0,
-  `utilisateur_id` INT NOT NULL,
+  `date_rdv` DATE NULL DEFAULT NULL,
+  `commentaire` VARCHAR(150) NULL DEFAULT NULL,
+  `traite` TINYINT(1) NOT NULL DEFAULT '0',
+  `utilisateur_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`, `utilisateur_id`),
-  INDEX `fk_rappel_utilisateur1_idx` (`utilisateur_id` ASC),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_rappel_utilisateur1_idx` (`utilisateur_id` ASC),
   CONSTRAINT `fk_rappel_utilisateur1`
     FOREIGN KEY (`utilisateur_id`)
     REFERENCES `reminder`.`utilisateur` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 10
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- utils/sql/2.generate_data.sql
+++ utils/sql/2.generate_data.sql
@@ -1,17 +1,35 @@
-SET SQL_SAFE_UPDATES = 0;
-DELETE FROM `reminder`.`rappel`;
-DELETE FROM `reminder`.`utilisateur`;
 
 
-INSERT INTO `reminder`.`utilisateur` (`id`, `nom`, `prenom`, `telephone`, `mail`, `discr`, `formation`, `annees_etudes`, `competences`, `pretentions`, `mobilite`, `date_embauche`, `dernier_ese`)
-VALUES (1, 'Trebaol', 'Gwenael', '0607467217', 'trebaol@mail.io', 'apsidien', 'Master TIIL', '5', 'Javascript', '50k', 'Bretagne', '25/05/2014', '25/05/2015');
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES
+(1,'Trebaol','Gwenaël','0607467217','trebaol@mail.io','apsidien','Master TIIL',5,'Javascript',50,'Bretagne',NULL,NULL,'0000-00-00','0000-00-00',NULL, NULL,NULL, NULL),
+(2,'Lechauve','Thomas','0101010101','lechauve@mail.io','independant','Master en élevage de ver de terre',-23,'PHP, Python',150000,'2 jambes','Bouuh t\'es moche !',0,'LA SOURCE !','1901-12-22', NULL, NULL,NULL,NULL),
+(3,'Ogor','Yohann','0102030405','ogor@mail.io','candidat','Master TIIL',5,'Java JEE',150,'Bretagne',NULL,NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(4,'Le Sanguinaire','Léodagan',NULL,'jme-la-taille@en-biseau-voyez.kmltt','independant',NULL,NULL,NULL,NULL,NULL,'Ah nan mais j\'vous jure hein, des tanches pareilles, on devrait les mettre sous verre !',1,NULL,NULL,NULL,NULL,NULL, NULL),
+(5,'Blaize','Père','0298632541','les-autres-intervalles@c-est-dla-merde.kmltt','apsidien','Aucune',5,NULL,NULL,NULL,'Non mais attendez, là, je crois qu\'on s\'est mal compris : vous avez une idée du temps qu\'il me faut pour tracer une lettre avec ces PUTAINS DE PLUMES ?!',1,NULL,NULL,NULL,NULL,NULL , NULL),
+(6,'Macoincoin','Jeremy','02986532','bizu@mail.io','apsidien','?',5,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL, NULL),
+(7,'Le blanche Fesse','Guenièvre','98653214','mefiez-vous@des-cons.kmltt','independant',NULL,10,NULL,NULL,NULL,'Comme vous me touchez pas, les choses de l’amour je m’assois dessus, et je parle au figuré, alors je m\'suis plongée dans la pâte d\'amande! Quand je vous regarde et que j\'vois comment vous m\'traitez, hein, je m\'dis que j’aurais meilleur compte d’aller d\'ici jusqu’à Rome à pieds pour en chercher parce que c’est finalement la meilleure chose qui me soit arrivée ….',1,NULL,NULL,NULL,NULL,NULL, NULL),
+(8,'Jean','Marc',NULL,'jean-marc@bidoch.on','candidat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(9,'Le Gallois','Perceval',NULL,'gros-faisan@du-sud.kmltt','candidat',NULL,NULL,NULL,NULL,NULL,'Bref, tout ça pour dire, que je voudrais bien qu’on me considère en tant que Tel.',NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(10,'De Vannes','Karadoc',NULL,'le-graal-par-ci@le-graal-par-la.kmltt','candidat',NULL,NULL,NULL,NULL,NULL,'De l’hypolipémie ! J’ai plus de gras dans le sang. Je vais me mettre à peler et à perdre mes cheveux…',NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(11,'Pendragon','Arthur',NULL,'je-suis-pas-romain@merde.kmltt','independant',NULL,NULL,NULL,NULL,NULL,'Et si je vous dis que vous êtes deux glands, là, vous avez du péremptoire. C’est vous qui voyez.',NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(12,'Le Jeune','Bohort',NULL,'je-suis-un-miserable@mecreants.kmltt','independant',NULL,NULL,NULL,NULL,NULL,'J\'irai me coucher quand vous m\'aurez juré qu\'il n\'y a pas dans cette forêt d\'animal plus dangereux que le lapin adulte !',NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(13,'L\'enchanteur','Merlin',NULL,'evidemment-c-est@sans-alcool.kmltt','independant',NULL,NULL,NULL,NULL,NULL,'Vous n’êtes pas le plus fort, Môssieur Élias ! Quand on confond un clafoutis et une part de clafoutis, on vient pas la ramener !',NULL,NULL,NULL,NULL,NULL,NULL, NULL),
+(14,'CALECA','Mael',NULL,NULL,'candidat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL, NULL);
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO `reminder`.`utilisateur` (`id`, `nom`, `prenom`, `telephone`, `mail`, `discr`, `formation`, `annees_etudes`, `competences`, `pretentions`, `mobilite`, `date_embauche`, `dernier_ese`)
-VALUES (2, 'Lechauve', 'Thomas', '0101010101', 'lechauve@mail.io', 'independant', 'Master TIIL', '5', 'PHP, Python', '80k', 'Brest', NULL, NULL);
-
-INSERT INTO `reminder`.`utilisateur` (`id`, `nom`, `prenom`, `telephone`, `mail`, `discr`, `formation`, `annees_etudes`, `competences`, `pretentions`, `mobilite`, `date_embauche`, `dernier_ese`)
-VALUES (3, 'Ogor', 'Yohann', '0102030405', 'ogor@mail.io', 'candidat', 'Master TIIL', '5', 'Java JEE', '150k', 'Bretagne', NULL, NULL);
-
-INSERT INTO `reminder`.`rappel`(date_rappel, date_rdv, commentaire, traite, utilisateur_id) VALUES ('2015-06-08', '2015-06-15', 'ESE', 0, 1);
-INSERT INTO `reminder`.`rappel`(date_rappel, date_rdv, commentaire, traite, utilisateur_id) VALUES ('2015-06-15', NULL, 'relance', 0, 2);
-INSERT INTO `reminder`.`rappel`(date_rappel, date_rdv, commentaire, traite, utilisateur_id) VALUES ('2015-06-20', '2015-06-25', '2 eme entretien', 0, 3);
+LOCK TABLES `rappel` WRITE;
+/*!40000 ALTER TABLE `rappel` DISABLE KEYS */;
+INSERT INTO `rappel` VALUES
+(1,'2015-06-08','2015-06-15','ESE',1,1),
+(2,'2015-06-15',NULL,'relance',1,2),
+(3,'2015-06-20','2015-06-25','2 eme entretien',0,3),
+(4,'2015-05-09','2015-05-10','Commentaire test pour voir jusqu\'a quelle longueur je peux aller et si ca s\'affiche bien sur le front end !!! o/ o/ o/',1,2),
+(5,'2015-06-19','2015-06-12','Huehuehuehuehuehuehuehue',1,2),
+(6,'2015-06-19',NULL,'huehuehuehue',0,1),
+(7,'2015-06-03',NULL,'Contacter le père blaize !',0,5),
+(8,'2015-06-05',NULL,'Moi, j\'serais vous, je vous écouterais... Non, moi, j\'serais nous, je vous...',0,9);
+/*!40000 ALTER TABLE `rappel` ENABLE KEYS */;
+UNLOCK TABLES;
